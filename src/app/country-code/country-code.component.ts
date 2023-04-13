a04d0b2f2dd3761f8305b366a1d2f3c7f7006b1c
rachnakafle <rachanakafle99@gmail.com>
2023-04-13 13:01:13 +0545

    Initial Commit

---
+++ src/app/country-code/country-code.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'country-code',
+  templateUrl: './country-code.component.html',
+  styleUrls: ['./country-code.component.css']
+})
+export class CountryCodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
